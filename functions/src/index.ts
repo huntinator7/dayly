97a88561b035f5816e928e41e864882a85662cd3
huntinator7 <huntinator7@gmail.com>
2020-12-23 16:46:19 -0700

    Added onUserCreate function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,6 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp(functions.config().firebase);
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +9,20 @@ import * as functions from 'firebase-functions';
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+export const db = admin.firestore();
+
+export const onUserCreate = functions.auth.user().onCreate((user) => {
+  db.collection("User")
+    .doc(user.uid)
+    .set({
+      uid: user.uid,
+      initialDate: new Date(),
+      dailyTasks: [],
+    })
+    .then(() => {
+      db.collection("User").doc(user.uid).collection("Day").doc("1").set({
+        tasks: [],
+      });
+    });
+});
